751a279dccf15174857eddbfa7e261e778c26f9e
Bill Spreitzer <you@example.com>
2021-03-08 02:17:42 -0600

    CRAP-4 local commit

--- src/main/java/com/williamspreitzer/craps/bets/BetFactory.java
+++ src/main/java/com/williamspreitzer/craps/bets/BetFactory.java
@@ -4,27 +4,6 @@ public class BetFactory {
 
 	private static Bet bet = null;
 	
-	
-	/*
-	 * public static Bet createBet(BetType type, byte number, short amount) { Bet
-	 * bet = null; switch(type) { case ALL_BET: bet = new AllBet(number, amount);
-	 * break; case ANY_SEVEN_BET: break; case COME_BET: bet = new ComeBet(amount);
-	 * break; case CRAPS_TWO_OR_TWELEVE_BET: bet = new CrapsTwoOrTweleveBet(number,
-	 * amount); break; case DONT_COME_BAR_TWELEVE_BET: break; case
-	 * DONT_PASS_BAR_TWELEVE_BET: break; case DOUBLES_BET: break; case FIELD_BET:
-	 * bet = new FieldBet(amount); break; case FIRE_BET: bet = new FireBet(number,
-	 * amount); break; case FIVE_NINE_ODDS_BET: break; case FIVE_NINE_PLACE_BET: bet
-	 * = new FiveNinePlaceBet(number, amount); break; case FOUR_TEN_HARDWAY_BET:
-	 * break; case FOUR_TEN_ODDS_BET: break; case FOUR_TEN_PLACE_BET: bet = new
-	 * FourTenPlaceBet(number, amount); break; case HORN_BET: break; case
-	 * PASS_LINE_BET: bet = new PassLineBet(amount); break; case
-	 * SIX_EIGHT_HARDWAY_BET: break; case SIX_EIGHT_ODDS_BET: break; case
-	 * SIX_EIGHT_PLACE_BET: bet = new SixEightPlaceBet(number, amount); break; case
-	 * SMALL_BET: bet = new SmallBet(number, amount); break; case TALL_BET: bet =
-	 * new TallBet(number, amount); break; case THREE_OR_ELEVEN_BET: break; } return
-	 * bet; }
-	 */
-	
 	@SuppressWarnings("incomplete-switch")
 	public static PropositionBet createPropsBet(BetType type, int betAmount) {
 		switch(type) {
