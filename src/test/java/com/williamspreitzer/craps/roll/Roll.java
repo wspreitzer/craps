4925fce0731e0496f604cb9a0e5a249daa55380c
Bill Spreitzer <you@example.com>
2021-03-24 23:33:51 -0500

    CRAP-3

---
+++ src/test/java/com/williamspreitzer/craps/roll/Roll.java
@@ -0,0 +1,11 @@
+package com.williamspreitzer.craps.roll;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class Roll {
+
+	MockedConstruction<Roll> mocked;
+	
+
+}
